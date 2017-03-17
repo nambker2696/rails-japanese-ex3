6192ba2ec799827fdfde286f5a39ad0d46155181
namnguyen <nguyenvannam20132696@gmail.com>
2017-03-17 07:38:43 +0000

    user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310075329) do
+ActiveRecord::Schema.define(version: 20170310090438) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
